48300ef44079eb0c91abc6598661231cbf9bfbdf
Lucas Duncan <lucasdonuts@gmail.com>
2022-06-30 07:43:39 -0500

    Cleaned up code a bit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@
 const form = document.getElementById("create-task-form")
 const ul = document.getElementById("tasks");
 const taskList = [];
-// const taskList = document.getElementById("tasks");
 
 document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", (event) => {
     event.preventDefault();
-    // description = document.getElementById("new-task-description").value;
     createTask();
   });
 
@@ -24,9 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     taskList.push(newTask);
-    // taskList.sort((a, b) => b.priority - a.priority);
     sortTasks();
-    // renderTask(newTask);
     renderTaskList();
     form.reset();
   }
@@ -75,89 +71,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // function renderTask(task) {
-  //   const li = document.createElement('li');
-  //   const button = document.createElement('button');
-
-  //   button.addEventListener("click", li.remove());
-
-  //   li.textContent = task.description.value;
-  //   li.style.color = task.color;
-  //   li.className = setClassName(li);
-  //   ul.append(li);
-  //   li.append(button);
-  //   console.log(li);
-  // }
+  function setClassName(item) {
+      if (item.priority == "3") {
+        return "high-priority"
+      } else if (item.priority == "2") {
+        return "mid-priority"
+      } else {
+        return "low-priority"
+      }
+  }
 });
-
-function setClassName(item) {
-    if (item.priority == "3") {
-      return "high-priority"
-    } else if (item.priority == "2") {
-      return "mid-priority"
-    } else {
-      return "low-priority"
-    }
-}
-
-// // for (let key in taskList) {
-  
-// // };
-
-// function sortTasks() {
-//   console.log(taskList)
-//   taskArray = [...document.querySelectorAll("#tasks li")];
-  
-//   for (let task of taskArray) {
-//     switch (task.className) {
-//       case "high-priority":
-//         task.value = 3;
-//         break;
-//       case "mid-priority":
-//         task.value = 2;
-//         break;
-//       case "low-priority":
-//         task.value = 1;
-//         break;
-//     }
-//   }
-
-//   taskArray.sort((a, b) => b.value - a.value);
-
-// }
-
-// function addNewTask(description) {
-//   const li = document.createElement("li");
-//   const priority = document.getElementById("priority");
-//   li.textContent = description;
-//   addDeleteButton(li);
-//   setColor(li, priority);
-//   taskList.append(li);
-//   li.id = description;
-//   document.getElementById("new-task-description").value = "";
-//   sortTasks();
-// }
-
-// function setColor(item, priority) {
-//   switch (priority.value) {
-//     case "high priority":
-//       item.style.color = "red";
-//       item.className = "high-priority";
-//       break;
-//     case "mid priority":
-//       item.style.color = "yellow";
-//       item.className = "mid-priority";
-//       break;
-//     default:
-//       item.style.color = "green";
-//       item.className = "low-priority";
-//       break;
-//   }
-// }
-
-// function addDeleteButton(listItem) {
-//   deleteButton = document.createElement("button")
-//   deleteButton.textContent = "X";
-//   listItem.append(deleteButton);
-//   deleteButton.addEventListener("click", e => e.target.parentElement.remove());
-// }
